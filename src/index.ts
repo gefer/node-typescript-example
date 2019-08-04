dd022f1c1769794a0383c2d22bfbab3e7df9ed0b
Felipe Langaro Savaris <felipe.savaris@intranet.maxiconsystems.com.br>
2019-08-04 13:08:02 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,17 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-
-import {createConnection} from "typeorm";
-
 import {createExpressServer, Header} from "routing-controllers";
 import {XMLController} from "./controller/XmlController";
 import {TabelaPrecoController} from "./controller/TabelaPrecoController";
-import * as app from "express";
 
-import * as cors from "cors";
 const Koa = require('koa');
 const corsDeMerda = require('@koa/cors');
 
 //Estabelece a conexão com o banco de dados
 createConnection().then(async connection => {
 
-    let API_URL = "http://localhost:3001";
 
     const app = new Koa();
-
-    const options:cors.CorsOptions = {
-        exposedHeaders: ["Access-Control-Allow-Origin", "*"],
-        allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
-        credentials: true,
-        methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-        origin: API_URL,
-        preflightContinue: false
-    };
 
     app.use(corsDeMerda());
 
