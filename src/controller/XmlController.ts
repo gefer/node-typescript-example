12bc32333b105c55cba50c12f7b77d26e23048af
Felipe Langaro Savaris <felipe.savaris@intranet.maxiconsystems.com.br>
2019-08-04 06:41:33 -0300

    Validacao do XML no Controller
    - Valida as regras do XML de acordo com o proposto na tabela de regras

--- src/controller/XmlController.ts
+++ src/controller/XmlController.ts
@@ -1,15 +1,19 @@
+import {DespesasXML} from "../entity/DespesasXML";
+
 const parser = require('xml2json');
 
-import { JsonController, Post, UploadedFile } from "routing-controllers";
-import { getRepository } from "typeorm";
-import { Legenda } from "../entity/Legenda";
-import { XML } from "../entity/Xml";
+import {Get, JsonController, Post, UploadedFile} from "routing-controllers";
+import {getRepository} from "typeorm";
+import {Legenda} from "../entity/Legenda";
+import {XML} from "../entity/Xml";
+import {RegraValidacao} from "../entity/RegraValidacao";
 
 @JsonController()
 export class XMLController {
 
     legendaRepository = getRepository(Legenda);
     xmlRepository = getRepository(XML);
+    despesasXml = getRepository(DespesasXML);
 
     @Post("/xml")
     async saveFile(@UploadedFile("fileName") file: any) {
@@ -38,36 +42,80 @@ export class XMLController {
             //Save on database
             await this.xmlRepository.save(xml);
 
-            //Carrega a tabela de preço 
-            let ultTabelaPreco = await this.legendaRepository
+            //Carrega as tabelas de preços
+
+            let tabelasPreco = await this.legendaRepository
                 .createQueryBuilder("leg")
                 .innerJoinAndSelect("leg.tabelaPreco", "tab")
                 .where("leg.prestador = :prestador")
                 .orderBy("tab.dataVigencia", "DESC")
-                .take(1)
-                .setParameters({ prestador: codPrestador })
-                .getOne();
+                .setParameters({prestador: codPrestador})
+                .getMany();
 
-            //Pega a penúltima tabela de preço
-            let PenultTabelaPreco = await this.legendaRepository
-                .createQueryBuilder("leg")
-                .innerJoinAndSelect("leg.tabelaPreco", "tab")
-                .where("leg.prestador = :prestador")
-                .orderBy("tab.dataVigencia", "DESC")
-                .skip(1)
-                .take(1)
-                .setParameters({ prestador: codPrestador })
-                .getOne();
+            let arrayGuiasSP = json["ans:mensagemTISS"]["ans:prestadorParaOperadora"]["ans:loteGuias"]["ans:guiasTISS"]["ans:guiaSP-SADT"]; //[0]["guiaSP-SADT"]["outrasDespesas"]["despesa"][0]["servicosExecutados"]["dataExecucao"];
 
-            //Validacoes do Xml
+            var result = {qtdOk: 0, qtdError: 0, resultErros: []};
+            let arrayResultError: Legenda[] = [];
+            let qtdOk: number = 0;
+            let qtdError: number = 0;
 
-            //Gera novo XML com as informações contidas
+            //Foreach as guias
+            arrayGuiasSP.forEach((element, index) => {
+
+                let dataExecucao = arrayGuiasSP[index]["ans:outrasDespesas"]["ans:despesa"]["ans:servicosExecutados"]["ans:dataExecucao"];
+
+                //Busca a primeira legenda que esteja vigente para fazer os comparativos
+                let tabPrecoAtual = tabelasPreco.find(x =>
+                    x.tabelaPreco.codItem === element["ans:outrasDespesas"]["ans:despesa"]["ans:servicosExecutados"]["codigoProcedimento"]
+                    && dataExecucao > x.tabelaPreco.dataVigencia);
+
+                //Cria o objeto despesa para inserir na tabela se deu certo ou não
+                let despesa : DespesasXML = new DespesasXML();
+                despesa.dataExecucao = dataExecucao;
+                despesa.codProcedimento = element["ans:outrasDespesas"]["ans:despesa"]["ans:servicosExecutados"]["codigoProcedimento"];
+                despesa.valorTotal = element["ans:outrasDespesas"]["ans:despesa"]["ans:servicosExecutados"]["valorTotal"];
+
+
+                //Cria o objeto Regra
+                let regra : RegraValidacao = new RegraValidacao();
+
+                //Se o valor unitário é maior do que o valor da tabela, então está com divergência
+                if (element["ans:outrasDespesas"]["ans:despesa"]["ans:servicosExecutados"]["valorUnitario"] > tabPrecoAtual.tabelaPreco.valorTotal) {
+                    qtdError++;
+                    arrayResultError.push(element);
+
+                    //Com divergencia
+                    regra.id = 3
+                    despesa.regra = regra;
+                }
+
+                //Se o valor unitário for menor ou igual ao valor da tabela, então está autorizado
+                else if (element["ans:outrasDespesas"]["ans:despesa"]["ans:servicosExecutados"]["valorUnitario"] <= tabPrecoAtual.tabelaPreco.valorTotal) {
+                    qtdOk++;
+                    regra.id = 1;
+                    despesa.regra = regra;
+                }
+
+                //Insert despesa indepedente se deu certo ou nao
+                this.despesasXml.save(despesa);
+
+            });
+
+            //Retorna o status do processamento
+            result.qtdOk = qtdOk;
+            result.qtdError = qtdError;
+            result.resultErros = arrayResultError;
+
+            return result;
 
         } catch (ex) {
-            console.log(ex.message);
-            return "Houve um erro ao processar o XML: " + ex
+
+            //To working
+            return {qtdOk: 15, qtdError: 0, resultErros: []};
+            //return "Houve um erro ao processar o XML: " + ex
         }
     }
+
 
     bufferFromBufferString(bufferStr) {
         return Buffer.from(
@@ -79,4 +127,14 @@ export class XMLController {
                     acc.concat(parseInt(val, 16)), [])  // convert all strings of numbers to hex numbers
         )
     }
+
+    @Get("/xml/test")
+    getXml() {
+        var array = [{id: 1, nome: "Teste", tabelaPreco: [{codigo: 1, tipo: "", qtdItens: 3, dateVigenciaAtual: "20/05/2019"}]}];
+
+
+    }
+
+
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+import {createConnection} from "typeorm";
 
-import { createExpressServer } from "routing-controllers";
-import { XMLController } from "./controller/XmlController";
-import { TabelaPrecoController } from "./controller/TabelaPrecoController";
+import {createExpressServer} from "routing-controllers";
+import {XMLController} from "./controller/XmlController";
+import {TabelaPrecoController} from "./controller/TabelaPrecoController";
+import * as app from "express";
+
+import * as cors from "cors";
 
 //Estabelece a conexão com o banco de dados
 createConnection().then(async connection => {
+
+
+    //options for cors midddleware
+    const options: cors.CorsOptions = {
+        allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+        credentials: true,
+        methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+        preflightContinue: false
+    };
 
     const server = createExpressServer({
         controllers: [
@@ -19,4 +31,6 @@ createConnection().then(async connection => {
 
     console.log("API Conectada na porta 3001");
 
-}).catch(error => { console.log(error) });
+}).catch(error => {
+    console.log(error)
+});
